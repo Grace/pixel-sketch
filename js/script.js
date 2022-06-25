154c47c2b09fbec6cd7def6c352c3ab01c42b3d9
Grace <grace@gracefulco.de>
2022-06-24 22:43:52 -0400

    Implement rainbow mode button.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 let gridContainer = document.querySelector('.grid-container');
 
 var selectedColor = 'black';
+
+function selectRandomColor() {
+    var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    return `#${randomColor}`;
+}
+
+const rainbowModeBtn = document.querySelector('#rainbow-mode-btn');
+rainbowModeBtn.addEventListener('click', (e) => {
+    let gridItems = document.querySelectorAll('.grid-item');
+    gridItems.forEach(item => item.removeEventListener('mouseover', changeColorOnHover));
+    gridItems.forEach(item => item.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = selectRandomColor();
+    }));
+});
+
 function changeColorOnHover(e) {
     console.log(e.target);
     e.target.style.backgroundColor = selectedColor;
@@ -9,11 +24,15 @@ function changeColorOnHover(e) {
 const newGridBtn = document.querySelector('#new-grid-btn');
 newGridBtn.addEventListener('click', (e) => {
     let input = parseInt(prompt("How many squares per side?"));
-    let gridContainer = document.querySelector('.grid-container');
-    gridContainer.style['grid-template-columns'] = `repeat(${input}, 1fr`;
-    createGrid(input);
+    if (input <= 100) {
+        let gridContainer = document.querySelector('.grid-container');
+        gridContainer.style['grid-template-columns'] = `repeat(${input}, 1fr`;
+        createGrid(input);
+    } else {
+        alert("Please enter a value less than or equal to 100.");
+    }
+    
 })
-
 
 function createGrid(dimensionNumber) {
     let gridItems = document.querySelectorAll('.grid-item');
